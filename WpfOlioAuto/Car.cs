7c90ce2df9bd3a078af243476d54a06e15c1de41
Jasmin <jasmin.kaalinpaa@gmail.com>
2018-12-11 14:22:57 +0200

    Add project files.

---
+++ WpfOlioAuto/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfOlioAuto
+{
+    public class Car
+    {
+        private int maxSpeed;
+
+        public int MaxSpeed {
+            get
+            {
+                return maxSpeed;
+            }
+
+            set
+            {
+                if (value > 0 && value < 400)
+                {
+                    maxSpeed = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }                                        // kirjoita prop ja sitten paina kaksi kertaa tabulaattoria
+
+        public string Color { get; set; }
+
+        public void SetMaxSpeed(int max)
+        {
+            if (max > 0 && max < 400)
+            {
+                maxSpeed = max;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+        }
+
+        public int GetMaxSpeed()
+        {
+            return maxSpeed;
+        }
+
+        public void StartEngine()
+        {
+
+        }
+
+        public void StopEngine()
+        {
+
+        }
+    }
+}
---
+++ WpfOlioAuto/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfOlioAuto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnKaynnista_Click(object sender, RoutedEventArgs e)
+        {
+            Car Ferrari = new Car();
+            Car Toyota = new Car();
+
+            Ferrari.Color = txtVari.Text;
+            Ferrari.MaxSpeed = int.Parse(txtHuippuNopeus.Text);
+            MessageBox.Show("Ferrarin väri on: " + Ferrari.Color + " ja huippunopeus on: " + Ferrari.MaxSpeed.ToString());
+
+            Toyota.Color = "Vihreä";
+            Toyota.SetMaxSpeed(175);
+            MessageBox.Show("Toyotan väri on: " + Toyota.Color + " ja huippunopeus on: " + Toyota.GetMaxSpeed().ToString());
+
+        }
+    }
+}
